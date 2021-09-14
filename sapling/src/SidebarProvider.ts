041ce94426e771c32c3c4c28b1d36a372176561a
Charles Gutwirth <charlesgutwirth@Jonathan-Gutwirth.local>
2021-09-14 14:41:53 -0700

    Connected webview to the sidebar and instantiated the app connection to the extension.
    Co-authored-by: Jordan Hisel <hiseljm@gmail.com>
    Co-authored-by: Lindsay Baird <lindsay.a.baird@gmail.com>
    Co-authored-by: Paul Coster <pllcoster@gmail.com>

---
+++ sapling/src/SidebarProvider.ts
@@ -0,0 +1,90 @@
+import * as vscode from "vscode";
+import { getNonce } from "./getNonce";
+
+export class SidebarProvider implements vscode.WebviewViewProvider {
+  _view?: vscode.WebviewView;
+  _doc?: vscode.TextDocument;
+
+  constructor(private readonly _extensionUri: vscode.Uri) {}
+
+  // this instantiates the connection to the webview
+  public resolveWebviewView(webviewView: vscode.WebviewView) {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+      localResourceRoots: [this._extensionUri],
+    };
+
+    // reaches out to the project file connecter function below
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+    // message section that will listen for messages sent from the React components to communicate with the extension
+    webviewView.webview.onDidReceiveMessage(async (data) => {
+      switch (data.type) {
+        case "onInfo": {
+          if (!data.value) {
+            return;
+          }
+          vscode.window.showInformationMessage(data.value);
+          break;
+        }
+        case "onError": {
+          if (!data.value) {
+            return;
+          }
+          vscode.window.showErrorMessage(data.value);
+          break;
+        }
+      }
+    });
+  }
+
+  // revive statement for the webview panel
+  public revive(panel: vscode.WebviewView) {
+    this._view = panel;
+  }
+
+  // paths and return statement that connects the webview to React project files
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    const styleResetUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+    );
+    const styleVSCodeUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+    );
+
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "out", "compiled/sidebar.js")
+    );
+    const styleMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "out", "compiled/sidebar.css")
+    );
+
+    // Use a nonce to only allow a specific script to be run.
+    const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+        -->
+        <meta http-equiv="Content-Security-Policy" content="default-src img-src https: data:; style-src 'unsafe-inline' ${
+      webview.cspSource
+      }; script-src 'nonce-${nonce}';">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link href="${styleResetUri}" rel="stylesheet">
+				<link href="${styleVSCodeUri}" rel="stylesheet">
+        <link href="${styleMainUri}" rel="stylesheet">
+			</head>
+      <body>
+        <h1>Hello World</h1>
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
+  }
+}
--- sapling/src/extension.ts
+++ sapling/src/extension.ts
@@ -1,25 +1,29 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { SidebarProvider } from './SidebarProvider';
 
-// this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sapling" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sapling.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Sapling!');
-	});
-
-	context.subscriptions.push(disposable);
+	// instantiating the sidebar webview
+  const sidebarProvider = new SidebarProvider(context.extensionUri);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      "sapling-sidebar",
+      sidebarProvider
+    )
+  );
+	// seting up a hotkey to refresh the extension without manual refresh
+	context.subscriptions.push(
+		vscode.commands.registerCommand('sapling.refresh', async () => {
+			// async call to close the sidebar
+			await vscode.commands.executeCommand('workbench.action.closeSidebar');
+			// async call to open the extension
+			await vscode.commands.executeCommand('workbench.view.extension.sapling-sidebar-view');
+			// open the webdev tools on create (ID for Open Webdev Tools)
+			setTimeout(() => {
+				vscode.commands.executeCommand('workbench.action.webview.openDeveloperTools');
+			}, 500);
+		})
+	);
 }
 
 // this method is called when your extension is deactivated
