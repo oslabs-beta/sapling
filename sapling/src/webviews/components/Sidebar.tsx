c98a7924e74b3de7a6ec92b7abc6ef09a8455cf7
Charles Gutwirth <charlesgutwirth@Jonathan-Gutwirth.local>
2021-09-16 21:06:11 -0700

    Figured out how to recursively call the Tree component inside of the li.
    Co-authored-by: Jordan Hisel <hiseljm@gmail.com>
    Co-authored-by: Lindsay Baird <lindsay.a.baird@gmail.com>
    Co-authored-by: Paul Coster <pllcoster@gmail.com>

--- sapling/src/webviews/components/Sidebar.tsx
+++ sapling/src/webviews/components/Sidebar.tsx
@@ -20,48 +20,101 @@ const testData: any = [
             "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/components/Main.jsx",
             "children": [],
             "depth": 2
+          },
+          {
+            "source": "Main.jsx",
+            "name": "TEST 1",
+            "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/components/Main.jsx",
+            "children": [],
+            "depth": 2
           }
         ],
         "depth": 1
       },
       {
-        "source": "random-test",
-        "name": "TEST 1",
-        "filename": "random-test",
+        "source": "test_0/components/App.jsx",
+        "name": "TEST 2",
+        "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/components/App.jsx",
         "children": [
           {
-            "source": "random-test",
-            "name": "TEST 2",
-            "filename": "random-test",
+            "source": "Main.jsx",
+            "name": "TEST 3",
+            "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/components/Main.jsx",
             "children": [],
             "depth": 2
           },
           {
-            "source": "random-test",
-            "name": "TEST 3",
-            "filename": "random-test",
-            "children": [],
+            "source": "Main.jsx",
+            "name": "TEST 4",
+            "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/components/Main.jsx",
+            "children": [
+              {
+                "source": "Main.jsx",
+                "name": "TEST 5",
+                "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/components/Main.jsx",
+                "children": [],
+                "depth": 3
+              },
+            ],
             "depth": 2
           }
         ],
         "depth": 1
-      }
-    ]
-  },
-  {
-    "name": "TEST 4",
-    "filename": "random-test",
-    "depth": 0,
-    "children": [
-      {
-        "source": "random-test",
-        "name": "TEST 5",
-        "filename": "random-test",
-        "children": [],
-        "depth": 1
       },
     ]
   },
+  {
+    "name": "TEST 6",
+    "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/index.js",
+    "depth": 0,
+    "children": [
+      {
+        "source": "test_0/components/App.jsx",
+        "name": "Test 7",
+        "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/components/App.jsx",
+        "children": [
+          {
+            "source": "Main.jsx",
+            "name": "TEST 8",
+            "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/components/Main.jsx",
+            "children": [],
+            "depth": 2
+          },
+        ],
+        "depth": 1
+      },
+      {
+        "source": "test_0/components/App.jsx",
+        "name": "TEST 9",
+        "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/components/App.jsx",
+        "children": [
+          {
+            "source": "Main.jsx",
+            "name": "TEST 10",
+            "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/components/Main.jsx",
+            "children": [],
+            "depth": 2
+          },
+          {
+            "source": "Main.jsx",
+            "name": "TEST 11",
+            "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/components/Main.jsx",
+            "children": [
+              {
+                "source": "Main.jsx",
+                "name": "TEST 12",
+                "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/components/Main.jsx",
+                "children": [],
+                "depth": 3
+              },
+            ],
+            "depth": 2
+          }
+        ],
+        "depth": 1
+      },
+    ]
+  }
 ];
 
 const Sidebar = () => {
@@ -83,3 +136,61 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+
+//   <ul className="tree">
+//     <li>
+//       <input type="checkbox" id="c1" />
+//       <label className="tree_label" htmlFor="c1" >Level 0</label>
+//       <ul>
+//         <li>
+//           <input type="checkbox" id="c2" />
+//           <label className="tree_label" htmlFor="c2">Level 1</label>
+//           <ul>
+//             <li><span className="tree_label">Level 2</span></li>
+//             <li><span className="tree_label">Level 2</span></li>
+//           </ul>
+//         </li>
+//         <li>
+//           <input type="checkbox" id="c3" />
+//           <label className="tree_label" htmlFor="c3">Looong level 1 <br/>label text <br/>with line-breaks</label>
+//           <ul>
+//             <li><span className="tree_label">Level 2</span></li>
+//             <li>
+//               <input type="checkbox" id="c4"/>
+//               <label className="tree_label" htmlFor="c4"><span className="tree_custom">Specified tree item view</span></label>
+//               <ul>
+//                 <li><span className="tree_label">Level 3</span></li>
+//               </ul>
+//             </li>
+//           </ul>
+//         </li>
+//       </ul>
+//     </li>
+//     <li>
+//       <input type="checkbox" id="c5" />
+//       <label className="tree_label" htmlFor="c5">Level 0</label>
+//       <ul>
+//         <li>
+//           <input type="checkbox" id="c6" />
+//           <label className="tree_label" htmlFor="c6">Level 1</label>
+//           <ul>
+//             <li><span className="tree_label">Level 2</span></li>
+//           </ul>
+//         </li>
+//         <li>
+//           <input type="checkbox" id="c7" />
+//           <label className="tree_label" htmlFor="c7">Level 1</label>
+//           <ul>
+//             <li><span className="tree_label">Level 2</span></li>
+//             <li>
+//               <input type="checkbox" id="c8" />
+//               <label className="tree_label" htmlFor="c8">Level 2</label>
+//               <ul>
+//                 <li><span className="tree_label">Level 3</span></li>
+//               </ul>
+//             </li>
+//           </ul>
+//         </li>
+//       </ul>
+//     </li>
+//   </ul>
--- sapling/src/webviews/components/Tree.tsx
+++ sapling/src/webviews/components/Tree.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import TreeNode from './TreeNode';
 
 const Tree = ({ data, first }: any) => {
-  console.log('data received inside of Tree:', data);
   const idRandomizer = () => {
     let text = "";
     const possible =
@@ -31,61 +30,3 @@ const Tree = ({ data, first }: any) => {
 };
 
 export default Tree;
-
-//   <ul className="tree">
-//     <li>
-//       <input type="checkbox" id="c1" />
-//       <label className="tree_label" htmlFor="c1" >Level 0</label>
-//       <ul>
-//         <li>
-//           <input type="checkbox" id="c2" />
-//           <label className="tree_label" htmlFor="c2">Level 1</label>
-//           <ul>
-//             <li><span className="tree_label">Level 2</span></li>
-//             <li><span className="tree_label">Level 2</span></li>
-//           </ul>
-//         </li>
-//         <li>
-//           <input type="checkbox" id="c3" />
-//           <label className="tree_label" htmlFor="c3">Looong level 1 <br/>label text <br/>with line-breaks</label>
-//           <ul>
-//             <li><span className="tree_label">Level 2</span></li>
-//             <li>
-//               <input type="checkbox" id="c4"/>
-//               <label className="tree_label" htmlFor="c4"><span className="tree_custom">Specified tree item view</span></label>
-//               <ul>
-//                 <li><span className="tree_label">Level 3</span></li>
-//               </ul>
-//             </li>
-//           </ul>
-//         </li>
-//       </ul>
-//     </li>
-//     <li>
-//       <input type="checkbox" id="c5" />
-//       <label className="tree_label" htmlFor="c5">Level 0</label>
-//       <ul>
-//         <li>
-//           <input type="checkbox" id="c6" />
-//           <label className="tree_label" htmlFor="c6">Level 1</label>
-//           <ul>
-//             <li><span className="tree_label">Level 2</span></li>
-//           </ul>
-//         </li>
-//         <li>
-//           <input type="checkbox" id="c7" />
-//           <label className="tree_label" htmlFor="c7">Level 1</label>
-//           <ul>
-//             <li><span className="tree_label">Level 2</span></li>
-//             <li>
-//               <input type="checkbox" id="c8" />
-//               <label className="tree_label" htmlFor="c8">Level 2</label>
-//               <ul>
-//                 <li><span className="tree_label">Level 3</span></li>
-//               </ul>
-//             </li>
-//           </ul>
-//         </li>
-//       </ul>
-//     </li>
-//   </ul>
