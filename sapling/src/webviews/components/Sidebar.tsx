9196347292225ea050a952e1af8993e2a8ff487f
Charles Gutwirth <charlesgutwirth@Jonathan-Gutwirth.local>
2021-09-16 19:11:21 -0700

    Working on parsing the data we receive to turn it into a tree.
    Co-authored-by: Jordan Hisel <hiseljm@gmail.com>
    Co-authored-by: Lindsay Baird <lindsay.a.baird@gmail.com>
    Co-authored-by: Paul Coster <pllcoster@gmail.com>

--- sapling/src/webviews/components/Sidebar.tsx
+++ sapling/src/webviews/components/Sidebar.tsx
@@ -1,12 +1,83 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import Tree from './Tree';
 
+const testData: any = [
+  {
+    "name": "index",
+    "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/index.js",
+    "depth": 0,
+    "children": [
+      {
+        "source": "test_0/components/App.jsx",
+        "name": "App",
+        "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/components/App.jsx",
+        "children": [
+          {
+            "source": "Main.jsx",
+            "name": "Main",
+            "filename": "/home/plcoster/Coding-Ubuntu/Codesmith/parser_tests/test_0/components/Main.jsx",
+            "children": [],
+            "depth": 2
+          }
+        ],
+        "depth": 1
+      },
+      {
+        "source": "random-test",
+        "name": "TEST 1",
+        "filename": "random-test",
+        "children": [
+          {
+            "source": "random-test",
+            "name": "TEST 2",
+            "filename": "random-test",
+            "children": [],
+            "depth": 2
+          },
+          {
+            "source": "random-test",
+            "name": "TEST 3",
+            "filename": "random-test",
+            "children": [],
+            "depth": 2
+          }
+        ],
+        "depth": 1
+      }
+    ]
+  },
+  {
+    "name": "TEST 4",
+    "filename": "random-test",
+    "depth": 0,
+    "children": [
+      {
+        "source": "random-test",
+        "name": "TEST 5",
+        "filename": "random-test",
+        "children": [],
+        "depth": 1
+      },
+    ]
+  },
+];
+
 const Sidebar = () => {
+  // const [treeData, setTreeData]: any = useState();
+  // useEffect(() => {
+  //   // listener for the postMessage parsed-data
+  //   // populate the tree based on the parsed-data value
+  //   console.log('test tree at start:', testData);
+  //   setTreeData(testData);
+  // }, []);
   return (
     <div className="sidebar">
       <Navbar />
-      <Tree />
+      <ul className="tree_beginning">
+        <Tree data={testData} first={true} />
+      </ul>
     </div>
   );
 };
--- sapling/src/webviews/components/Tree.tsx
+++ sapling/src/webviews/components/Tree.tsx
@@ -1,70 +1,91 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import TreeNode from './TreeNode';
 
-const Tree = () => {
-  useEffect(() => {
-    // listener for the postMessage parsed-data
-    // populate the tree based on the parsed-data value
-  }, []);
+const Tree = ({ data, first }: any) => {
+  console.log('data received inside of Tree:', data);
+  const idRandomizer = () => {
+    let text = "";
+    const possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < 32; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
   return (
-    <ul className="tree">
-      <li>
-        <input type="checkbox" id="c1" />
-        <label className="tree_label" htmlFor="c1" >Level 0</label>
+    <>
+      {first ? data.map((tree: any, i: any) => {
+          const uniqueId = idRandomizer();
+          return <TreeNode node={tree} htmlId={uniqueId} />;
+        }):
         <ul>
-          <li>
-            <input type="checkbox" id="c2" />
-            <label className="tree_label" htmlFor="c2">Level 1</label>
-            <ul>
-              <li><span className="tree_label">Level 2</span></li>
-              <li><span className="tree_label">Level 2</span></li>
-            </ul>
-          </li>
-          <li>
-            <input type="checkbox" id="c3" />
-            <label className="tree_label" htmlFor="c3">Looong level 1 <br/>label text <br/>with line-breaks</label>
-            <ul>
-              <li><span className="tree_label">Level 2</span></li>
-              <li>
-                <input type="checkbox" id="c4"/>
-                <label className="tree_label" htmlFor="c4"><span className="tree_custom">Specified tree item view</span></label>
-                <ul>
-                  <li><span className="tree_label">Level 3</span></li>
-                </ul>
-              </li>
-            </ul>
-          </li>
+          {data.map((tree: any, i: any) => {
+            const uniqueId = idRandomizer();
+            return <TreeNode node={tree} htmlId={uniqueId} />;
+          })}
         </ul>
-      </li>
-      <li>
-        <input type="checkbox" id="c5" />
-        <label className="tree_label" htmlFor="c5">Level 0</label>
-        <ul>
-          <li>
-            <input type="checkbox" id="c6" />
-            <label className="tree_label" htmlFor="c6">Level 1</label>
-            <ul>
-              <li><span className="tree_label">Level 2</span></li>
-            </ul>
-          </li>
-          <li>
-            <input type="checkbox" id="c7" />
-            <label className="tree_label" htmlFor="c7">Level 1</label>
-            <ul>
-              <li><span className="tree_label">Level 2</span></li>
-              <li>
-                <input type="checkbox" id="c8" />
-                <label className="tree_label" htmlFor="c8">Level 2</label>
-                <ul>
-                  <li><span className="tree_label">Level 3</span></li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </li>
-    </ul>
+      }
+    </>
   );
 };
 
 export default Tree;
+
+//   <ul className="tree">
+//     <li>
+//       <input type="checkbox" id="c1" />
+//       <label className="tree_label" htmlFor="c1" >Level 0</label>
+//       <ul>
+//         <li>
+//           <input type="checkbox" id="c2" />
+//           <label className="tree_label" htmlFor="c2">Level 1</label>
+//           <ul>
+//             <li><span className="tree_label">Level 2</span></li>
+//             <li><span className="tree_label">Level 2</span></li>
+//           </ul>
+//         </li>
+//         <li>
+//           <input type="checkbox" id="c3" />
+//           <label className="tree_label" htmlFor="c3">Looong level 1 <br/>label text <br/>with line-breaks</label>
+//           <ul>
+//             <li><span className="tree_label">Level 2</span></li>
+//             <li>
+//               <input type="checkbox" id="c4"/>
+//               <label className="tree_label" htmlFor="c4"><span className="tree_custom">Specified tree item view</span></label>
+//               <ul>
+//                 <li><span className="tree_label">Level 3</span></li>
+//               </ul>
+//             </li>
+//           </ul>
+//         </li>
+//       </ul>
+//     </li>
+//     <li>
+//       <input type="checkbox" id="c5" />
+//       <label className="tree_label" htmlFor="c5">Level 0</label>
+//       <ul>
+//         <li>
+//           <input type="checkbox" id="c6" />
+//           <label className="tree_label" htmlFor="c6">Level 1</label>
+//           <ul>
+//             <li><span className="tree_label">Level 2</span></li>
+//           </ul>
+//         </li>
+//         <li>
+//           <input type="checkbox" id="c7" />
+//           <label className="tree_label" htmlFor="c7">Level 1</label>
+//           <ul>
+//             <li><span className="tree_label">Level 2</span></li>
+//             <li>
+//               <input type="checkbox" id="c8" />
+//               <label className="tree_label" htmlFor="c8">Level 2</label>
+//               <ul>
+//                 <li><span className="tree_label">Level 3</span></li>
+//               </ul>
+//             </li>
+//           </ul>
+//         </li>
+//       </ul>
+//     </li>
+//   </ul>
