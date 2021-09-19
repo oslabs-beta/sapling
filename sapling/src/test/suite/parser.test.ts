e3ff14e767877f3be5f05f2d3d5f02ed8ac89281
Paul <paul@megs-imac.attlocal.net>
2021-09-19 15:28:51 -0700

    Added initial parser test files to extension test suite
    Co-authored-by: Charles Gutwirth <charlesgutwirth@gmail.com>
    Co-authored-by: Jordan Hisel <hiseljm@gmail.com>
    Co-authored-by: Lindsay Baird <lindsay.a.baird@gmail.com>

--- sapling/src/test/suite/parser.test.ts
+++ sapling/src/test/suite/parser.test.ts
@@ -1,20 +1,17 @@
 import * as assert from 'assert';
+import { saplingParse } from '../../parser';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite('Parser Test Suite', () => {
 
-	test('Sample test', () => {
+	test('Parser Test', () => {
+		console.log(saplingParse);
+		assert.strictEqual(typeof saplingParse('../test_apps/test_0/index.js'), 'object');
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
 	});
-
-	test('Sample test 2', () => {
-		assert.strictEqual(1, 1);
-	});
-
 });
