3d43188b55293cf4f2794ee49ed057f1e21d5c5a
PLCoster <58193305+PLCoster@users.noreply.github.com>
2021-10-05 12:06:29 -0700

    Updates to website ready for build
    Co-authored-by: Charles Gutwirth <charlesgutwirth@gmail.com>
    Co-authored-by: Jordan Hisel <hiseljm@gmail.com>
    Co-authored-by: Lindsay Baird <lindsay.a.baird@gmail.com>

--- website/components/Contributors.jsx
+++ website/components/Contributors.jsx
@@ -9,60 +9,56 @@ const Contributors = () => {
 
       <div className="row">
 
-        <div className="person col-xs-3 col-sm-3 col-md-3">
+        <div className="person col-sm-3">
           <div className="card">
-            <img src="https://avatars.githubusercontent.com/u/87096293" className="card-img-top"/>
-            <div className="card-body">
-              <img className="profile-small" src="https://avatars.githubusercontent.com/u/87096293" />
-              <div className="person-info text-center">
-                <h5 className="card-title">Charles Gutwirth</h5>
-                <a href="https://github.com/charlesgutwirth" target="_blank">
-                  <Image src={githubIcon}/>
+            <Image src="https://avatars.githubusercontent.com/u/87096293" className="card-img-top" alt="Charles Gutwirth Picture" width={200} height={200}/>
+            <div className="card-body person-info text-center">
+              <h5 className="card-title">Charles Gutwirth</h5>
+              <div className="profile-links">
+                <a href="https://github.com/charlesgutwirth" target="_blank" rel="noreferrer">
+                  <Image className="profile-link" src={githubIcon} width={40} height={40} alt="Github OctoCat Logo"/>
                 </a>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="person col-xs-3 col-sm-3 col-md-3">
+        <div className="person col-sm-3">
           <div className="card">
-            <img src="https://avatars.githubusercontent.com/u/64238519" className="card-img-top"/>
-            <div className="card-body">
-              <img className="profile-small" src="https://avatars.githubusercontent.com/u/64238519" />
-              <div className="person-info text-center">
-                <h5 className="card-title">Jordan Hisel</h5>
-                <a href="https://github.com/jo-cella" target="_blank">
-                  <Image src={githubIcon}/>
+            <Image src="https://avatars.githubusercontent.com/u/64238519" className="card-img-top" alt="Jordan Hisel Picture" width={200} height={200}/>
+            <div className="card-body person-info text-center">
+              <h5 className="card-title">Jordan Hisel</h5>
+              <div className="profile-links">
+                <a href="https://github.com/jo-cella" target="_blank" rel="noreferrer">
+                  <Image className="profile-link" src={githubIcon} width={40} height={40}  alt="Github OctoCat Logo"/>
                 </a>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="person col-xs-3 col-sm-3 col-md-3">
+        <div className="person col-sm-3">
           <div className="card">
-            <img src="https://avatars.githubusercontent.com/u/81602150" className="card-img-top"/>
-            <div className="card-body">
-              <img className="profile-small" src="https://avatars.githubusercontent.com/u/81602150" />
-              <div className="person-info text-center">
-                <h5 className="card-title">Lindsay Baird</h5>
-                <a href="https://github.com/labaird" target="_blank">
-                  <Image src={githubIcon}/>
+            <Image src="https://avatars.githubusercontent.com/u/81602150" className="card-img-top" alt="Lindsay Baird Picture" width={200} height={200}/>
+            <div className="card-body person-info text-center">
+              <h5 className="card-title">Lindsay Baird</h5>
+              <div className="profile-links">
+                <a href="https://github.com/labaird" target="_blank" rel="noreferrer">
+                  <Image className="profile-link" src={githubIcon} width={40} height={40}  alt="Github OctoCat Logo"/>
                 </a>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="person col-xs-3 col-sm-3 col-md-3">
+        <div className="person col-sm-3">
           <div className="card">
-            <img src="https://avatars.githubusercontent.com/u/58193305" className="card-img-top"/>
-            <div className="card-body">
-              <img className="profile-small" src="https://avatars.githubusercontent.com/u/58193305" />
-              <div className="person-info text-center">
-                <h5 className="card-title">Paul Coster</h5>
-                <a href="https://github.com/PLCoster" target="_blank">
-                  <Image src={githubIcon}/>
+            <Image src="https://avatars.githubusercontent.com/u/58193305" className="card-img-top" alt="Paul Coster Picture" width={200} height={200}/>
+            <div className="card-body person-info text-center">
+              <h5 className="card-title">Paul Coster</h5>
+              <div className="profile-links">
+                <a href="https://github.com/PLCoster" target="_blank" rel="noreferrer">
+                  <Image className="profile-link" src={githubIcon} width={40} height={40}  alt="Github OctoCat Logo"/>
                 </a>
               </div>
             </div>
