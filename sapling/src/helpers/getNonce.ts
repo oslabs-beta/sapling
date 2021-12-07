5a3362ebe594f9112f894a5c5de7823aeeaf3177
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2021-12-06 18:19:32 -0800

    fix: resolve expand toggle bug
    - Flaw in getNonce implementation caused issues in webview:
      - expand toggle behaved erratically
      - when build-tree was called from status bar on file in tree, previously highlighted node would remain bold even after tree content was updated
    - Adding enzyme tests to CI seems like a good next step

--- sapling/src/helpers/getNonce.ts
+++ sapling/src/helpers/getNonce.ts
@@ -3,6 +3,7 @@ const NONCE_LENGTH: number = 32;
 
 export function getNonce(): string {
   const nonce = new Array(NONCE_LENGTH).fill('0');
-  nonce.map(() => VALID_CHARACTERS.charAt(Math.floor(Math.random() * VALID_CHARACTERS.length)));
-  return nonce.join('');
+  return nonce
+    .map(() => VALID_CHARACTERS.charAt(Math.floor(Math.random() * VALID_CHARACTERS.length)))
+    .join('');
 }
