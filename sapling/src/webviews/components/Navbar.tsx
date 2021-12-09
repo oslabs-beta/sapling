3056f2565852a45ca2778fabe339c6ab2a54a3b4
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2021-12-08 16:21:03 -0800

    style: use synthetic default imports for react

--- sapling/src/webviews/components/Navbar.tsx
+++ sapling/src/webviews/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 // imports for the icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
--- sapling/src/webviews/components/Tree.tsx
+++ sapling/src/webviews/components/Tree.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 // eslint-disable-next-line import/no-cycle
 import TreeNode from './TreeNode';
