d2e72c8d62292b9e7f0fa325d89d1ae89c433c2a
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2021-12-09 20:19:22 -0800

    refactor(extension): add new types for parser

---
+++ sapling/src/types/ImportData.ts
@@ -0,0 +1,5 @@
+export type ImportData = {
+  importPath: string;
+  importName: string;
+  importAlias?: string;
+};
--- sapling/src/types/ImportObj.ts
+++
@@ -1,5 +0,0 @@
-// ImportObj type, used by SaplingParser
-// ImportObj holds data about imports in the current JS/TS file
-export type ImportObj = {
-  [key : string]: {importPath: string, importName: string}
-};
