a56c479018abd3ce9a0f01a1f0a5e7a0ea287189
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2021-12-19 16:41:49 -0800

    refactor(parser)!: group methods into static modules
    - Low cohesion red-flag: No reference to this.tree/this(SaplingParser class) in any ASTParser or ImportParser method.
    - Suggestion: decouple tree state from parser and make parser class static and stateless (i.e. no class members)

--- sapling/src/SaplingParser.ts
+++ sapling/src/SaplingParser.ts
@@ -69,7 +69,7 @@ export class SaplingParser {
     };
 
     this.tree = root;
-    this.parser(root);
+    ASTParser.parser(root);
     return this.tree;
   }
 
@@ -122,7 +122,7 @@ export class SaplingParser {
           this.#traverseTree(getChildNodes, child);
         });
 
-        const newNode = this.parser(node);
+        const newNode = ASTParser.parser(node);
 
         this.#traverseTree(matchExpand, newNode);
 
@@ -160,9 +160,11 @@ export class SaplingParser {
       this.#traverseTree(callback, childNode);
     });
   }
+}
 
+const ASTParser = {
   // Recursively builds the React component tree structure starting from root node
-  private parser(componentTree: Tree): Tree {
+  parser(componentTree: Tree): Tree {
     // If import is a node module, do not parse any deeper
     if (!['\\', '/', '.'].includes(componentTree.importPath[0])) {
       componentTree.thirdParty = true;
@@ -211,42 +213,174 @@ export class SaplingParser {
     }
 
     // Find imports in the current file, then find child components in the current file
-    const imports = this.getImports(ast.program.body);
+    const imports = ImportParser.parse(ast.program.body);
 
     // Get any JSX Children of current file:
-    componentTree.children = this.getJSXChildren(ast.tokens, imports, componentTree);
+    componentTree.children = ASTParser.getJSXChildren(ast.tokens, imports, componentTree);
 
     // Check if current node is connected to the Redux store
-    componentTree.reduxConnect = this.checkForRedux(ast.tokens, imports);
+    componentTree.reduxConnect = ASTParser.checkForRedux(ast.tokens, imports);
 
     // Recursively parse all child components
-    componentTree.children.forEach((child) => this.parser(child));
+    componentTree.children.forEach((child) => ASTParser.parser(child));
 
     return componentTree;
-  }
+  },
 
+  validateFilePath(filePath: string): string {
+    const fileArray: string[] = [];
+    let parsedFileName = '';
+    // Handles Next.js component and other third-party imports
+    try {
+      fileArray.push(...fs.readdirSync(path.dirname(filePath)));
+    } catch {
+      return filePath;
+    }
+    // Checks that file exists and appends file extension to path if not given in import declaration
+    parsedFileName =
+      fileArray.find((str) => new RegExp(`${path.basename(filePath)}\\.[jt]sx?$`).test(str)) || '';
+    if (parsedFileName.length) return filePath + path.extname(parsedFileName);
+    return filePath;
+  },
+
+  getChildNodes(
+    imports: Record<string, ImportData>,
+    astToken: Token,
+    props: Record<string, boolean>,
+    parent: Tree,
+    children: Record<string, Tree>
+  ): Record<string, Tree> {
+    if (children[astToken.value]) {
+      children[astToken.value].count += 1;
+      children[astToken.value].props = { ...children[astToken.value].props, ...props };
+    } else {
+      const moduleIdentifier = imports[astToken.value].importPath;
+      const name = imports[astToken.value].importName;
+      const filePath = ASTParser.validateFilePath(
+        path.resolve(path.dirname(parent.filePath), moduleIdentifier)
+      );
+      // Add tree node to childNodes if one does not exist
+      children[astToken.value] = {
+        id: getNonce(),
+        name,
+        fileName: path.basename(filePath),
+        filePath,
+        importPath: moduleIdentifier,
+        expanded: false,
+        depth: parent.depth + 1,
+        thirdParty: false,
+        reactRouter: false,
+        reduxConnect: false,
+        count: 1,
+        props,
+        children: [],
+        parentList: [parent.filePath].concat(parent.parentList),
+        error: '',
+      };
+    }
+    return children;
+  },
+
+  // Finds JSX React Components in current file
+  getJSXChildren(
+    astTokens: Array<Token>,
+    imports: Record<string, ImportData>,
+    parentNode: Tree
+  ): Array<Tree> {
+    let childNodes: Record<string, Tree> = {};
+    let props: Record<string, boolean> = {};
+    let token: Token;
+
+    for (let i = 0; i < astTokens.length; i++) {
+      // Case for finding JSX tags eg <App .../>
+      if (
+        astTokens[i].type.label === 'jsxTagStart' &&
+        astTokens[i + 1].type.label === 'jsxName' &&
+        imports[astTokens[i + 1].value]
+      ) {
+        token = astTokens[i + 1];
+        props = ASTParser.getJSXProps(astTokens, i + 2);
+        childNodes = ASTParser.getChildNodes(imports, token, props, parentNode, childNodes);
+
+        // Case for finding components passed in as props e.g. <Route component={App} />
+      } else if (
+        astTokens[i].type.label === 'jsxName' &&
+        (astTokens[i].value === 'component' || astTokens[i].value === 'children') &&
+        imports[astTokens[i + 3].value]
+      ) {
+        token = astTokens[i + 3];
+        childNodes = ASTParser.getChildNodes(imports, token, props, parentNode, childNodes);
+      }
+    }
+
+    return Object.values(childNodes);
+  },
+
+  // Extracts prop names from a JSX element
+  getJSXProps(astTokens: Array<Token>, j: number): Record<string, boolean> {
+    const props: Record<string, boolean> = {};
+    while (astTokens[j].type.label !== 'jsxTagEnd') {
+      if (astTokens[j].type.label === 'jsxName' && astTokens[j + 1].value === '=') {
+        props[astTokens[j].value] = true;
+      }
+      j += 1;
+    }
+    return props;
+  },
+
+  // Checks if current Node is connected to React-Redux Store
+  checkForRedux(astTokens: Array<Token>, imports: Record<string, ImportData>): boolean {
+    // Check that react-redux is imported in this file (and we have a connect method or otherwise)
+    let reduxImported = false;
+    let connectAlias;
+    Object.keys(imports).forEach((key) => {
+      if (imports[key].importPath === 'react-redux' && imports[key].importName === 'connect') {
+        reduxImported = true;
+        connectAlias = key;
+      }
+    });
+
+    if (!reduxImported) {
+      return false;
+    }
+
+    // Check that connect method is invoked and exported in the file
+    for (let i = 0; i < astTokens.length; i += 1) {
+      if (
+        astTokens[i].type.label === 'export' &&
+        astTokens[i + 1].type.label === 'default' &&
+        astTokens[i + 2].value === connectAlias
+      ) {
+        return true;
+      }
+    }
+    return false;
+  },
+};
+
+const ImportParser = {
   /* Extracts Imports from current file
    * https://github.com/babel/babel/blob/main/packages/babel-parser/ast/spec.md
    * https://github.com/babel/babel/blob/main/packages/babel-types/src/ast-types/generated/index.ts
    */
-  private getImports(body: Array<ASTNode>): Record<string, ImportData> {
+  parse(body: Array<ASTNode>): Record<string, ImportData> {
     return body
       .filter((astNode) => isImportDeclaration(astNode) || isVariableDeclaration(astNode))
       .reduce((accum: Record<string, ImportData>, declaration) => {
         return isImportDeclaration(declaration)
-          ? Object.assign(accum, this.parseImportDeclaration(declaration))
+          ? Object.assign(accum, ImportParser.parseImportDeclaration(declaration))
           : isVariableDeclaration(declaration)
-          ? Object.assign(accum, this.parseVariableDeclaration(declaration))
+          ? Object.assign(accum, ImportParser.parseVariableDeclaration(declaration))
           : accum;
       }, {});
-  }
+  },
 
   /* Import Declarations: 
    * e.g. import foo from "mod"
    '.source': name/path of imported module
    https://github.com/babel/babel/blob/main/packages/babel-parser/ast/spec.md#Imports
    */
-  private parseImportDeclaration(declaration: ImportDeclaration): Record<string, ImportData> {
+  parseImportDeclaration(declaration: ImportDeclaration): Record<string, ImportData> {
     const output: Record<string, ImportData> = {};
     let importName = '';
     let importAlias: string | undefined;
@@ -291,7 +425,7 @@ export class SaplingParser {
       };
     });
     return output;
-  }
+  },
 
   /* Imports Inside Variable Declarations (and current support status): 
    * [x] e.g. const foo = require("module");
@@ -303,7 +437,7 @@ export class SaplingParser {
    * [v] e.g. const foo = React.lazy(() => import('./module'));
    https://github.com/babel/babel/blob/main/packages/babel-parser/ast/spec.md#VariableDeclaration
    */
-  private parseVariableDeclaration(declaration: VariableDeclaration): Record<string, ImportData> {
+  parseVariableDeclaration(declaration: VariableDeclaration): Record<string, ImportData> {
     const output: Record<string, ImportData> = {};
     let importName = '';
     let importAlias: string | undefined;
@@ -377,7 +511,7 @@ export class SaplingParser {
       /* React lazy loading import
        * e.g. const foo = React.lazy(() => import('./module'));
        */
-      importPath = this.parseNestedDynamicImports(declarator);
+      importPath = ImportParser.parseNestedDynamicImports(declarator);
       if (importPath.length && isIdentifier(declarator.id)) {
         importName = declarator.id.name;
         output[importAlias || importName] = {
@@ -388,11 +522,11 @@ export class SaplingParser {
       }
     });
     return output;
-  }
+  },
 
   // TODO: Explicit parsing of nested Import CallExpression in ArrowFunctionExpression body
   // TODO: Support AwaitExpression, Promise.resolve(), then() chains for dynamic imports
-  private parseNestedDynamicImports(ast: ASTNode): string {
+  parseNestedDynamicImports(ast: ASTNode): string {
     const recurse = (node: ASTNode): string | void => {
       if (isCallExpression(node) && isImport(node.callee) && isStringLiteral(node.arguments[0])) {
         return node.arguments[0].value;
@@ -408,135 +542,11 @@ export class SaplingParser {
       }
     };
     return recurse(ast) || '';
-  }
+  },
+};
 
-  private validateFilePath(filePath: string): string {
-    const fileArray: string[] = [];
-    let parsedFileName = '';
-    // Handles Next.js component and other third-party imports
-    try {
-      fileArray.push(...fs.readdirSync(path.dirname(filePath)));
-    } catch {
-      return filePath;
-    }
-    // Checks that file exists and appends file extension to path if not given in import declaration
-    parsedFileName =
-      fileArray.find((str) => new RegExp(`${path.basename(filePath)}\\.[jt]sx?$`).test(str)) || '';
-    if (parsedFileName.length) return filePath + path.extname(parsedFileName);
-    return filePath;
-  }
-
-  private getChildNodes(
-    imports: Record<string, ImportData>,
-    astToken: Token,
-    props: Record<string, boolean>,
-    parent: Tree,
-    children: Record<string, Tree>
-  ): Record<string, Tree> {
-    if (children[astToken.value]) {
-      children[astToken.value].count += 1;
-      children[astToken.value].props = { ...children[astToken.value].props, ...props };
-    } else {
-      const moduleIdentifier = imports[astToken.value].importPath;
-      const name = imports[astToken.value].importName;
-      const filePath = this.validateFilePath(
-        path.resolve(path.dirname(parent.filePath), moduleIdentifier)
-      );
-      // Add tree node to childNodes if one does not exist
-      children[astToken.value] = {
-        id: getNonce(),
-        name,
-        fileName: path.basename(filePath),
-        filePath,
-        importPath: moduleIdentifier,
-        expanded: false,
-        depth: parent.depth + 1,
-        thirdParty: false,
-        reactRouter: false,
-        reduxConnect: false,
-        count: 1,
-        props,
-        children: [],
-        parentList: [parent.filePath].concat(parent.parentList),
-        error: '',
-      };
-    }
-    return children;
-  }
-
-  // Finds JSX React Components in current file
-  private getJSXChildren(
-    astTokens: Array<Token>,
-    imports: Record<string, ImportData>,
-    parentNode: Tree
-  ): Array<Tree> {
-    let childNodes: Record<string, Tree> = {};
-    let props: Record<string, boolean> = {};
-    let token: Token;
-
-    for (let i = 0; i < astTokens.length; i++) {
-      // Case for finding JSX tags eg <App .../>
-      if (
-        astTokens[i].type.label === 'jsxTagStart' &&
-        astTokens[i + 1].type.label === 'jsxName' &&
-        imports[astTokens[i + 1].value]
-      ) {
-        token = astTokens[i + 1];
-        props = this.getJSXProps(astTokens, i + 2);
-        childNodes = this.getChildNodes(imports, token, props, parentNode, childNodes);
-
-        // Case for finding components passed in as props e.g. <Route component={App} />
-      } else if (
-        astTokens[i].type.label === 'jsxName' &&
-        (astTokens[i].value === 'component' || astTokens[i].value === 'children') &&
-        imports[astTokens[i + 3].value]
-      ) {
-        token = astTokens[i + 3];
-        childNodes = this.getChildNodes(imports, token, props, parentNode, childNodes);
-      }
-    }
-
-    return Object.values(childNodes);
-  }
-
-  // Extracts prop names from a JSX element
-  private getJSXProps(astTokens: Array<Token>, j: number): Record<string, boolean> {
-    const props: Record<string, boolean> = {};
-    while (astTokens[j].type.label !== 'jsxTagEnd') {
-      if (astTokens[j].type.label === 'jsxName' && astTokens[j + 1].value === '=') {
-        props[astTokens[j].value] = true;
-      }
-      j += 1;
-    }
-    return props;
-  }
-
-  // Checks if current Node is connected to React-Redux Store
-  private checkForRedux(astTokens: Array<Token>, imports: Record<string, ImportData>): boolean {
-    // Check that react-redux is imported in this file (and we have a connect method or otherwise)
-    let reduxImported = false;
-    let connectAlias;
-    Object.keys(imports).forEach((key) => {
-      if (imports[key].importPath === 'react-redux' && imports[key].importName === 'connect') {
-        reduxImported = true;
-        connectAlias = key;
-      }
-    });
-
-    if (!reduxImported) {
-      return false;
-    }
-
-    // Check that connect method is invoked and exported in the file
-    for (let i = 0; i < astTokens.length; i += 1) {
-      if (
-        astTokens[i].type.label === 'export' &&
-        astTokens[i + 1].type.label === 'default' &&
-        astTokens[i + 2].value === connectAlias
-      ) {
-        return true;
-      }
-    }
-    return false;
-  }
-}
+// TODO: Follow import source paths and parse Export{Named,Default,All}Declarations
+// See: https://github.com/babel/babel/blob/main/packages/babel-parser/ast/spec.md#exports
+// Necessary for handling...
+// barrel files, namespace imports, default import + namespace/named imports, require invocations/method calls, ...
+const ExportParser = {};
